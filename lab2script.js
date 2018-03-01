24052444f4abe57a27175e128ccf1b3edf2e7fa1
rikard <rikard.hohlfalt@gmail.com>
2018-03-01 15:54:15 +0100

    adds modify and delete book

--- lab2script.js
+++ lab2script.js
@@ -1,10 +1,11 @@
 const url = 'https://www.forverkliga.se/JavaScript/api/crud.php?';
 const key = 'hC3jF'
+let success = false;
 
 function formatBooks(arr) {
     let str = "";
     for (i = 0; i < arr.length; i++) {
-        str += "<div class='table-cell'>" + arr[i].title + " by " + arr[i].author + "</div>";
+        str += "<div class='table-cell'>" + arr[i].id + ". " + arr[i].title + " by " + arr[i].author + "</div>";
     }
     return str;
 }
@@ -18,7 +19,6 @@ function fetchData() {
                     response.status);
                 return;
             }
-
             // Examine the text in the response
             response.json().then(function (data) {
                 console.log(data);
@@ -40,6 +40,68 @@ function fetchData() {
 
 function addData() {
     var request = new Request(url + 'op=insert&key=' + key + '&title=' + prompt('Enter title') + '&author=' + prompt('Enter author'), {
+        method: 'post',
+    });
+    fetch(request).then(
+        function (response) {
+            if (response.status !== 200) {
+                console.log('Looks like there was a problem. Status Code: ' +
+                    response.status);
+                return;
+            }
+
+            // Examine the text in the response
+            response.json().then(function (data) {
+                console.log(data);
+                if (data.status === "success") {
+                    document.getElementById("demo").innerHTML =
+                        data.status;
+                }
+                else {
+                    document.getElementById("demo").innerHTML =
+                        data.status + ' ' + data.message;
+                }
+            });
+        }
+    )
+        .catch(function (err) {
+            console.log('Fetch Error :-S', err);
+        });
+}
+
+function modifyData() {
+    var request = new Request(url + 'op=update&key=' + key + '&id=' + prompt('Enter id') + '&title=' + prompt('Enter title') + '&author=' + prompt('Enter author'), {
+        method: 'post',
+    });
+    fetch(request).then(
+        function (response) {
+            if (response.status !== 200) {
+                console.log('Looks like there was a problem. Status Code: ' +
+                    response.status);
+                return;
+            }
+
+            // Examine the text in the response
+            response.json().then(function (data) {
+                console.log(data);
+                if (data.status === "success") {
+                    document.getElementById("demo").innerHTML =
+                        data.status;
+                }
+                else {
+                    document.getElementById("demo").innerHTML =
+                        data.status + ' ' + data.message;
+                }
+            });
+        }
+    )
+        .catch(function (err) {
+            console.log('Fetch Error :-S', err);
+        });
+}
+
+function deleteData() {
+    var request = new Request(url + 'op=delete&key=' + key + '&id=' + prompt('Enter id'), {
         method: 'post',
     });
     fetch(request).then(
