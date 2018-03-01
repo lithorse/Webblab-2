1b2d1ac9c2b91ad9577ad0aca076c099b1d5d878
rikard <rikard.hohlfalt@gmail.com>
2018-03-01 14:18:11 +0100

    Adds html, js, css

---
+++ lab2script.js
@@ -0,0 +1,70 @@
+const url = 'https://www.forverkliga.se/JavaScript/api/crud.php?';
+const key = 'hC3jF'
+
+function formatBooks(arr) {
+    let str = "";
+    for (i = 0; i < arr.length; i++) {
+        str += "<div class='table-cell'>" + arr[i].title + " by " + arr[i].author + "</div>";
+    }
+    return str;
+}
+
+function fetchData() {
+    var request = new Request(url + 'op=select&key=' + key);
+    fetch(request).then(
+        function (response) {
+            if (response.status !== 200) {
+                console.log('Looks like there was a problem. Status Code: ' +
+                    response.status);
+                return;
+            }
+
+            // Examine the text in the response
+            response.json().then(function (data) {
+                console.log(data);
+                if (data.status === "success") {
+                    document.getElementById("demo").innerHTML =
+                        formatBooks(data.data);
+                }
+                else {
+                    document.getElementById("demo").innerHTML =
+                        data.status + ' ' + data.message;
+                }
+            });
+        }
+    )
+        .catch(function (err) {
+            console.log('Fetch Error :-S', err);
+        });
+}
+
+function addData() {
+    var request = new Request(url + 'op=insert&key=' + key + '&title=' + prompt('Enter title') + '&author=' + prompt('Enter author'), {
+        method: 'post',
+    });
+    fetch(request).then(
+        function (response) {
+            if (response.status !== 200) {
+                console.log('Looks like there was a problem. Status Code: ' +
+                    response.status);
+                return;
+            }
+
+            // Examine the text in the response
+            response.json().then(function (data) {
+                console.log(data);
+                if (data.status === "success") {
+                    document.getElementById("demo").innerHTML =
+                        data.status;
+                }
+                else {
+                    document.getElementById("demo").innerHTML =
+                        data.status + ' ' + data.message;
+                }
+            });
+        }
+    )
+        .catch(function (err) {
+            console.log('Fetch Error :-S', err);
+        });
+}
